21486f61ce48f09b3f108a801fa98f5d42a8d3d5
Abdullah Al Masud <masudctg00@gmail.com>
2022-02-11 15:40:36 +0600

    error handled

--- js/banking.js
+++ js/banking.js
@@ -15,34 +15,50 @@ function updateTotalField(totalFieldId, amount) {
     totalField.innerText = newTotalField
 }
 
+function updateBalance(amount, isAdd) {
+    const balanceTotalField = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotalField.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText); // num
+    if (isAdd == true) {
+        const newBalannceTotal = previousBalanceTotal + amount;
+        balanceTotalField.innerText = newBalannceTotal
+    } else {
+        const newBalannceTotal = previousBalanceTotal - amount;
+        balanceTotalField.innerText = newBalannceTotal
+    }
+}
+
+function getCurrentBalance() {
+    const balanceTotalField = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotalField.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText); // num
+    return previousBalanceTotal
+}
+
 /* Deposit button event */
 document.getElementById('deposit-button').addEventListener('click', function() {
     // Deposit
     const newDepositAmount = getInpuValue('deposit-input'); // function call
-
-    // update deposit field
-    updateTotalField('deposit-total', newDepositAmount) // function call
-
-    // Balance
-    const balanceTotalField = document.getElementById('balance-total');
-    const previousBalanceTotalText = balanceTotalField.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceTotalText); // num
-    const newBalannceTotal = previousBalanceTotal + newDepositAmount;
-    balanceTotalField.innerText = newBalannceTotal
+    if (newDepositAmount > 0) {
+        // update deposit field
+        updateTotalField('deposit-total', newDepositAmount) // function call
+            // update Balance
+        updateBalance(newDepositAmount, true);
+    }
 })
 
 /* Withdraw button event */
 document.getElementById('withdraw-button').addEventListener('click', function() {
     // withdraw
     const newWithdrawAmount = getInpuValue('withdraw-input') // function call
-
-    // update deposit field
-    updateTotalField('withdraw-total', newWithdrawAmount) // function call
-
-    // balance
-    const balanceTotalField = document.getElementById('balance-total');
-    const previousBalanceTotalText = balanceTotalField.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceTotalText); // num
-    const newBalannceTotal = previousBalanceTotal - newWithdrawAmount;
-    balanceTotalField.innerText = newBalannceTotal
+    const currentBalance = getCurrentBalance();
+    if (newWithdrawAmount > 0 && newWithdrawAmount < currentBalance) {
+        // update deposit field
+        updateTotalField('withdraw-total', newWithdrawAmount) // function call
+            // update balance
+        updateBalance(newWithdrawAmount, false)
+    }
+    if (newWithdrawAmount > currentBalance) {
+        console.log("You dont have enoung money")
+    }
 })
